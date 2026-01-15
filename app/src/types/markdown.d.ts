6fef878089fcdfd28be0c2cca8aa6b4cb7a753c3
Igor Tatarnikov <igor.tatarnikov@gmail.com>
2026-01-15 16:10:08 +0000

    Clean up JS files

--- app/src/types/markdown.d.ts
+++
@@ -1,5 +0,0 @@
-// declared module for importing markdown files
-declare module '*.md' {
-  const value: string;
-  export default value;
-}
--- backend/src/fetchers/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './discussions';
-export * from './issues';
-export * from './meta';
-export * from './organization';
-export * from './repository';
-export * from './downloads_pepy';
